0c336053557a0a523d78b3a9a3ec4488f2406e51
giammy90901 <138612799+giammy90901@users.noreply.github.com>
2024-02-17 13:49:23 +0100

    Update tabelline.py

--- tabelline.py
+++ tabelline.py
@@ -2,3 +2,4 @@ numero = int(input("Inserisci un numero intero: "))
 for i in range(1, 11):
     risultato = numero * i
     print(f"{numero} x {i} = {risultato}" )
+    #ho usato chatGpt perchè non ricordavo a cosa servisse la i in python
